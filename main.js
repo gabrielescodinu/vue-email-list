4c128283de5ed9a6cf19b0382baf75e7f5ce3637
gabriele <gabrielescodinu96@gmail.com>
2021-01-08 15:39:24 +0100

    Generate emails
    Can generate 10 random emails with api.

--- main.js
+++ main.js
@@ -3,18 +3,14 @@
 let app = new Vue ({
   el: "#app",
   data: {
-    newMail: "",
     mailList: [],
   },
   mounted(){
     for (var i = 0; i < 10; i++) {
       axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
       .then(resp => {
-        //console.log(resp.data.response);
-        let mail = resp.data.response;
-        this.mailList = mail;
+        this.mailList.push(resp.data.response)
       })
     }
-    console.log(mailList);
   }
 })
